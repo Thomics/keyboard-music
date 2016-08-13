9d5274fb4eda9f427c47c34c67bc2fcbb29daa31
Thomics <contot@gmail.com>
2016-08-13 01:16:52 -0700

    connected angular components

--- public/app/keyboard.controller.js
+++ public/app/keyboard.controller.js
@@ -5,22 +5,38 @@
     .module('keyboardApp')
     .controller('KeyboardController', KeyboardController);
 
-  KeyboardController.$inject = [];
+  KeyboardController.$inject = ['KeyboardService'];
 
-  function KeyboardController() {
+  function KeyboardController(KeyboardService) {
 
     var vm = this;
 
+    //vm.getSound = KeyboardService.getSound;
 
-    activate();
-
-
-    ///////////////////////////////////////////////////////////////
+    vm.sound;
 
 
-    function activate() {
+    function play() {
+      var audio = document.getElementById("audio");
+      audio.play();
 
     }
+
+    $(document).keypress(function(key) {
+      console.log(key.which);
+
+      vm.sound = KeyboardService.sound[key.which];
+
+      console.log(vm.sound);
+
+      if(key.which == 49) {
+
+        play();
+
+      }
+
+    });
+
 
 
   }
--- public/app/keyboard.service.js
+++ public/app/keyboard.service.js
@@ -14,8 +14,24 @@
     vm.getSound = getSound;
 
 
+    vm.sound = {
+      '48' : 'sounds/snare.wav',    //0
+      '49' : 'sounds/high-hat.wav', //1
+      '50' : 'sounds/high-hat.wav', //2
+      '51' : 'sounds/high-hat.wav', //3
+      '52' : 'sounds/high-hat.wav', //4
+      '53' : 'sounds/high-hat.wav', //5
+      '54' : 'sounds/high-hat.wav', //6
+      '55' : 'sounds/high-hat.wav', //7
+      '56' : 'sounds/high-hat.wav', //8
+      '57' : 'sounds/high-hat.wav'  //9
+    };
+
+
     function getSound(keyNum) {
-      //Match keyNum to object, and return audio file.
+
+      return vm.sound[keyNum];
+
     }
     
   }
