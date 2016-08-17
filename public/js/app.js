70e52e4b4a813d6550d8502ca88bca650d8f6151
Thomics <contot@gmail.com>
2016-08-16 22:12:24 -0700

    changing around the files

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
+'use strict';
 $(document).ready(function() {
 
   var keyArr = {start: 0, end: 0, time:[], key:[]};
-  var d = new Date();
   var recording = false;
 
 
@@ -11,17 +11,26 @@ $(document).ready(function() {
     ion.sound.play(key);
     changeKeyStyle(key);
 
+    console.log(key);
+    recordNotes(key);
 
+  });
+
+
+  function recordNotes(key) {
+    if(key === '#space' && recording) {
+
+      recording = !recording;
+
+      startRecord();
+    }
     if(recording) {
-
       var time = new Date();
       keyArr.time.push(time.getTime() - keyArr.start);
       keyArr.key.push(key);
-
     }
+  }
 
-
-  });
 
   //Mouse click
   $(document).on("click", '.keys-container', function (event) {
@@ -42,21 +51,30 @@ $(document).ready(function() {
     changeKeyStyle('#stop-metronome');
   });
 
-  $('#start-record').on('click', function() {
-    keyArr.start = d.getTime();
+
+  $('#start-record').on('click', startRecord);
+
+  $('#stop-record').on('click', stopRecord);
+
+
+  function startRecord() {
+    var time = new Date();
+    keyArr.start = time.getTime();
     recording = true;
     changeKeyStyle('#start-record');
-    $('.recording-text').css({'text-decoration' : 'none'});
     $('.recording').css({'background': '#83d0c9'});
-  });
+  }
 
-  $('#stop-record').on('click', function() {
-    keyArr.end = d.getTime();
+  function stopRecord() {
+    var time = new Date();
+    keyArr.end = time.getTime();
     recording = false;
     changeKeyStyle('#stop-record');
-    $('.recording-text').css({'text-decoration' : 'line-through'});
     $('.recording').css({'background': '#800080'});
-  });
+  }
+
+
+
 
 
   /**
@@ -69,6 +87,7 @@ $(document).ready(function() {
       $(id).css({'-webkit-filter': '', 'filter': ''});
     }, 200);
   }
+
 
   $('#play-record').on('click', function() {
     playback();
@@ -86,102 +105,12 @@ $(document).ready(function() {
     }, pauseTime, key);
   }
 
-  function playback() {
 
+  function playback() {
     for (var i = 0; i < keyArr.time.length; i++ ) {
       playbackTimer(i);
     }
-
   }
 
-
-
-  ion.sound({
-    sounds: [
-      //Row 1 - 0
-      {name: "Perc1",    alias: '#zero', volume:.7},  //key: 0, ascii: 48
-      {name: "Kick1",    alias: '#one'},   //key: 1, ascii: 49
-      {name: "Kick2",    alias: '#two'},   //key: 2, ascii: 50
-      {name: "Kick3",    alias: '#three'}, //key: 3, ascii: 51
-      {name: "Kick4",    alias: '#four'},  //key: 4, ascii: 52
-      {name: "Clap1",    alias: '#five'},  //key: 5, ascii: 53
-      {name: "Clap2",    alias: '#six'},   //key: 6, ascii: 54
-      {name: "Clap3",    alias: '#seven'}, //key: 7, ascii: 55
-      {name: "Clap4",    alias: '#eight'}, //key: 8, ascii: 56
-      {name: "Perc10",   alias: '#nine'},  //key: 9, ascii: 57
-
-      //Row qwerty
-      {name: "Perc9",    alias: '#q'},     //key: q, ascii: 113
-      {name: "Perc7",    alias: '#w'},     //key: w, ascii: 119
-      {name: "Perc3",    alias: '#e'},     //key: e, ascii: 101
-      {name: "Perc4",    alias: '#r'},     //key: r, ascii: 114
-      {name: "Perc6",    alias: '#t'},     //key: t, ascii: 116
-      {name: "Perc8",    alias: '#y'},     //key: y, ascii: 121
-      {name: "Perc2",    alias: '#u'},     //key: u, ascii: 117
-      {name: "Perc5",    alias: '#i'},     //key: i, ascii: 105
-      {name: "triangle", alias: '#o'},     //key: o, ascii: 111
-      {name: "snap",     alias: '#p'},     //key: p, ascii: 112
-
-      //Row asdf
-      {name: "synth5",   alias: '#a'},     //key: a, ascii: 97
-      {name: "synth6",   alias: '#s'},     //key: s, ascii: 115
-      {name: "synth7",   alias: '#d'},     //key: d, ascii: 100
-      {name: "synth8",   alias: '#f'},     //key: f, ascii: 102
-      {name: "synth9",   alias: '#g'},     //key: g, ascii: 103
-      {name: "bass",     alias: '#h'},     //key: h, ascii: 104
-      {name: "ah",       alias: '#j'},     //key: j, ascii: 106
-      {name: "do",       alias: '#k'},     //key: k, ascii: 107
-      {name: "uhh",      alias: '#l'},     //key: l, ascii: 108
-      {name: "oh",       alias: '#colon'}, //key: ;, ascii: 59
-
-
-      {name: "metronome",   alias: '#play-metronome', loop: true, volume: .3}
-
-    ],
-
-    //general configuration
-    path: "public/sounds/",
-    preload: true,
-    multiplay: true,
-    volume: 1
-  });
-
-
-  var keyNums = {
-    '49'  : '#one',
-    '50'  : '#two',
-    '51'  : '#three',
-    '52'  : '#four',
-    '53'  : '#five',
-    '54'  : '#six',
-    '55'  : '#seven',
-    '56'  : '#eight',
-    '57'  : '#nine',
-    '48'  : '#zero',
-    '113' : '#q',
-    '119' : '#w',
-    '101' : '#e',
-    '114' : '#r',
-    '116' : '#t',
-    '121' : '#y',
-    '117' : '#u',
-    '105' : '#i',
-    '111' : '#o',
-    '112' : '#p',
-    '91'  : '#lbracket',
-    '93'  : '#rbracket',
-    '97'  : '#a',
-    '115' : '#s',
-    '100' : '#d',
-    '102' : '#f',
-    '103' : '#g',
-    '104' : '#h',
-    '106' : '#j',
-    '107' : '#k',
-    '108' : '#l',
-    '59'  : '#colon',
-    '39'  : '#apostrophe',
-    '32'  : '#space'
-  };
 
 });
